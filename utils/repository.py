aa07b9278a4c1254d4700deb62008a1bf9181b94
a1473838623 <1473838623@qq.com>
2026-01-09 13:20:50 +0800

    fix oom

--- utils/repository.py
+++ utils/repository.py
@@ -40,34 +40,35 @@ class TSRepository(object):
         class_pred = torch.index_select(self.targets, 0, sample_pred)
         return class_pred
 
-    def mine_nearest_neighbors(self, topk, calculate_accuracy=False):
+    def mine_nearest_neighbors(self, topk, calculate_accuracy=True):
+        # mine the topk nearest neighbors for every sample
         features = self.features.cpu().numpy()
-        n_samples = features.shape[0]
-
-        knn_model = NearestNeighbors(n_neighbors=topk + 1,
+        knn_model = NearestNeighbors(n_neighbors=features.shape[0],
                                      algorithm='brute',
                                      n_jobs=-1)
         knn_model.fit(features)
-        distances, indices = knn_model.kneighbors(features)
 
-        k_nearest_neighbours = indices[:, 1:topk + 1]  # 排除自身
+        distances, indices = knn_model.kneighbors(features, return_distance=True)
+        k_furthest_neighbours = []
+        k_nearest_neighbours = []
+        for i in range(features.shape[0]):
+            # sort the neighbours based on their distance to the point
+            sorted_indices = np.argsort(distances[i])
+            # get the k furthest neighbours for each point
+            k_furthest_neighbours.append(indices[i][sorted_indices[-topk:]])
+            k_nearest_neighbours.append(indices[i][sorted_indices[1:topk + 1]])
 
-        knn_full = NearestNeighbors(n_neighbors=n_samples,
-                                    algorithm='brute',
-                                    n_jobs=-1)
-        knn_full.fit(features)
-        _, indices_full = knn_full.kneighbors(features)
-        k_furthest_neighbours = indices_full[:, -topk:]
-
+        # evaluate
         if calculate_accuracy:
             targets = self.targets.cpu().numpy()
-            neighbor_targets = targets[k_nearest_neighbours]  # (n, topk)
-            anchor_targets = targets[:, np.newaxis]  # (n, 1) 广播
+            neighbor_targets = np.take(targets, indices[:, 1:], axis=0)  # Exclude sample itself for eval
+            anchor_targets = np.repeat(targets.reshape(-1, 1), topk, axis=1)
             accuracy = np.mean(neighbor_targets == anchor_targets)
 
             return k_furthest_neighbours, k_nearest_neighbours, accuracy
 
-        return k_furthest_neighbours, k_nearest_neighbours
+        else:
+            return k_furthest_neighbours, k_nearest_neighbours
 
 
     def furthest_nearest_neighbors(self, topk):
